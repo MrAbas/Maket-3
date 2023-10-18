0a04c27f0172c1780aabf67c3e178eb3d8fe9b5a
MrAbas <abasov2016@list.ru>
2023-10-18 16:32:44 +0300

    сделали кнопку бургер активной

---
+++ js/index.js
@@ -0,0 +1,8 @@
+let burger__menu = document.getElementById("burger__menu");
+let burger__content = document.getElementById("burger__content");
+
+burger__menu.onclick = function () {
+  burger__content.classList.toggle("burger__content");
+
+  burger__menu.classList.toggle("burger__menu__cross");
+};
--- js/script.js
+++
@@ -1,6 +0,0 @@
-let burger__content = document.getElementsByClassName(burger__content);
-let burger__menu = document.getElementsByClassName(burger__menu);
-
-burger__menu.onclick = function () {
-  burger__menu.classlist.toggle("burger__content");
-};
