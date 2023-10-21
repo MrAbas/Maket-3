870eaa6631c0efc8c76085d2f9b5fd871cafadbe
MrAbas <abasov2016@list.ru>
2023-10-21 16:44:23 +0300

    добавил к поиску анимацию

--- js/index.js
+++ js/index.js
@@ -3,10 +3,7 @@ const burger__content = document.getElementById("burger__content");
 
 const button__search = document.getElementById("button__search");
 const closed_search = document.getElementById("closed_search");
-const btn_input = document.getElementById("btn_input");
-const show__container__search = document.getElementById(
-  "show__container__search"
-);
+const container__search = document.getElementById("container__search");
 
 burger__menu.onclick = function () {
   burger__content.classList.toggle("burger__content");
@@ -14,13 +11,9 @@ burger__menu.onclick = function () {
 };
 
 button__search.onclick = function () {
-  btn_input.classList.remove("hidden");
-  closed_search.classList.remove("hidden");
-  show__container__search.classList.add("show__container__search");
+  container__search.classList.add("container__search_show");
 };
 
 closed_search.onclick = function () {
-  btn_input.classList.add("hidden");
-  closed_search.classList.add("hidden");
-  show__container__search.classList.remove("show__container__search");
+  container__search.classList.remove("container__search_show");
 };
